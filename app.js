0e45aa4fb2339ff9faaeda3756d7661a7f69bde6
matheusgomes2020 <matheusgomez2013@gmail.com>
2023-04-21 15:41:07 -0300

    compose page

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+let posts = [];
+
 
 
 
@@ -30,6 +32,8 @@ app.get("/", function(req, res) {
 
   res.render("home", {startingContent: homeStartingContent});
 
+  console.log( posts )
+
 });
 
 app.get("/about", function(req, res) {
@@ -43,6 +47,28 @@ app.get("/contact", function(req, res) {
   res.render("contact", {contactContent: contactContent});
 
 });
+
+app.get("/compose", function(req, res) {
+
+  res.render("compose");
+
+});
+
+app.post("/compose", function(req, res){
+
+  const post = {
+
+    title: req.body.postTitle,
+    content: req.body.postBody
+
+  };
+
+  posts.push(post);
+
+  res.redirect("/")
+
+});
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
